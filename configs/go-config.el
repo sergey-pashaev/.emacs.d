52bb49f368fb3a4e655e1cf5a7224e8078897ff7
Sergey Pashaev <sergey.pashaev@gmail.com>
2013-10-22 17:29:28 +0400

    go eldoc added

--- configs/go-config.el
+++ configs/go-config.el
@@ -1,6 +1,11 @@
 ;;; go-config.el
 
+(require 'ac-config)
+
 (require-or-install 'go-mode)
 (require-or-install 'go-autocomplete)
+(require-or-install 'go-eldoc)
+
+(add-hook 'go-mode-hook 'go-eldoc-setup)
 
 (provide 'go-config)
