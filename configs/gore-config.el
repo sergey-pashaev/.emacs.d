7655880444b098a88a9e2b190427d2ed04e5ae3b
Sergey Pashaev <sergey.pashaev@gmail.com>
2013-10-28 16:26:38 +0400

    Use gore-mode from melpa

--- configs/gore-config.el
+++ configs/gore-config.el
@@ -1,7 +1,6 @@
 ;;; gore-config.el
 
-(add-to-list 'load-path "~/github/gore-mode/")
-(require 'gore-mode)
+(require-or-install 'gore-mode)
 (setq gore-bin-path "~/workspace/go/bin/gore")
 
 (provide 'gore-config)
