7ba0ce85db75ededab122aed507f26b26f5f09e5
Sergey Pashaev <sergey.pashaev@gmail.com>
2013-11-28 11:40:05 +0400

    Minor c-lang-config update

--- configs/c-lang-config.el
+++ configs/c-lang-config.el
@@ -3,5 +3,9 @@
 (require-or-install 'c-eldoc)
 
 (add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+(add-hook 'c++-mode-hook 'c-turn-on-eldoc-mode)
+
+(setq c-default-style "k&r"
+      c-basic-offset 4)
 
 (provide 'c-lang-config)
