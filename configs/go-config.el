e568ee207ac6483a82885184e6c8fc551db2ae16
Sergey Pashaev <sergey.pashaev@gmail.com>
2014-02-21 13:35:52 +0400

    golang: tab width and useful packages in comments

--- configs/go-config.el
+++ configs/go-config.el
@@ -1,5 +1,14 @@
 ;;; go-config.el
 
+;; mysql driver
+;;go get -u github.com/go-sql-driver/mysql
+
+;; dev utils
+;;go get -u github.com/dougm/goflymake
+;;go get -u github.com/nsf/gocode
+;;go get -u code.google.com/p/rog-go/exp/cmd/godef
+;;go get -u github.com/sriram-srinivasan/gore
+
 (require 'ac-config)
 
 (require-or-install 'go-mode)
@@ -10,6 +19,7 @@
 
 (defun my-go-local-bindings()
   (progn
+    (setq tab-width 4)
     (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
     (local-set-key (kbd "C-c i") 'go-goto-imports)))
 
