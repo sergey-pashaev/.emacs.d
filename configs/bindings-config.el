1bb7c9b938d974b4e9acfb09324ec2bc0d5c7f15
Sergey Pashaev <sergey.pashaev@gmail.com>
2014-02-21 13:30:36 +0400

    Invoke autocomplete only by hand

--- configs/bindings-config.el
+++ configs/bindings-config.el
@@ -16,7 +16,7 @@
 (global-set-key (kbd "C-<f6>") 'whitespace-mode)
 (global-set-key (kbd "C-<f12>") 'toggle-truncate-lines)
 (global-set-key (kbd "C-<backspace>") 'backward-kill-word)
-(global-set-key (kbd "M-/") 'hippie-expand)
+;(global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (provide 'bindings-config)
