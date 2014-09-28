1505c5b07e392ff6710432a07cecce2193844a46
Sergey Pashaev <sergey.pashaev@gmail.com>
2014-09-28 13:04:37 +0400

    ac config added and helm config update

--- configs/normal.el
+++ configs/normal.el
@@ -72,8 +72,47 @@
 (require 'helm-config)
 
 (use-package helm
-  :bind
-  (("M-?" . helm-projectile)
-   ("C-c !" . helm-flycheck)))
+  :init
+  (progn
+    (setq helm-quick-update                     t ; do not display invisible candidates
+	  helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+	  helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+	  helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+	  helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+	  helm-ff-file-name-history-use-recentf t)
+
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+    (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)	; make TAB works in terminal
+    (define-key helm-map (kbd "C-z")  'helm-select-action)		; list actions using C-z
+
+    (when (executable-find "curl")
+      (setq helm-google-suggest-use-curl-p t))
+
+    (helm-mode 1))
+    :bind
+    (("M-?" . helm-projectile)
+     ("C-c !" . helm-flycheck)))
+
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
 
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+
+;; auto-complete
+(require-package 'auto-complete)
+(require 'auto-complete-config)
+
+(use-package auto-complete
+  :init
+  (progn
+    (ac-config-default)
+
+    (setq ac-auto-start nil)
+    (setq ac-auto-show-menu t)
+    (setq ac-ignore-case 'smart)
+
+    (define-key ac-mode-map [(meta return)] 'auto-complete)))
