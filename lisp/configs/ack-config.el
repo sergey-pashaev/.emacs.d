fc18757589e8d5a1389e52d09aee7cf93cc11046
Sergey Pashaev <sergey.pashaev@gmail.com>
2015-01-12 09:58:58 +0400

    ack-config added

---
+++ lisp/configs/ack-config.el
@@ -0,0 +1,12 @@
+;;; ack-config.el
+
+(add-to-list 'load-path "/usr/bin/ack-grep")
+
+(require 'ack-and-a-half)
+
+(defalias 'ack 'ack-and-a-half)
+(defalias 'ack-same 'ack-and-a-half-same)
+(defalias 'ack-find-file 'ack-and-a-half-find-file)
+(defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+
+(provide 'ack-config)
