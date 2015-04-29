d77060f4024a1c75ff50bcbc3e6100d523778eed
Sergey Pashaev <sergey.pashaev@gmail.com>
2015-04-29 16:56:30 +0400

    bindings update

--- lisp/configs/bindings-config.el
+++ lisp/configs/bindings-config.el
@@ -25,8 +25,31 @@
 
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "M-/") 'dabbrev-expand)
+(global-set-key (kbd "s-/") 'hippie-expand)
+
+(setq hippie-expand-try-functions-list
+      '(try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
 
 (global-unset-key (kbd "C-x C-c"))
 (global-unset-key (kbd "C-x 5 0"))
+
+(global-set-key (kbd "M--") 'psv/goto-match-paren)
+(global-set-key (kbd "C-M-=") 'psv/diff-current-buffer-with-file)
+
+(global-set-key (kbd "M-[") 'backward-paragraph)
+(global-set-key (kbd "M-]") 'forward-paragraph)
+
+(global-set-key (kbd "M-s o") 'occur)
+(global-set-key (kbd "M-i") 'helm-imenu)
+(global-set-key (kbd "s-`") 'smartscan-symbol-replace)
 
 (provide 'bindings-config)
