1dcbf10779353584e4ee120b1217cab6ffaf0462
Sergey Pashaev <sergey.pashaev@gmail.com>
2015-07-31 18:35:24 +0400

    edit as root functions added

--- lisp/utils/editing-utils.el
+++ lisp/utils/editing-utils.el
@@ -292,4 +292,22 @@ Position the cursor at its beginning, according to the current mode."
   (save-excursion
     (sort-lines nil beg end)))
 
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'editing-utils)
