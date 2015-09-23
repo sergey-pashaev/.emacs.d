a9c8f00700c857c484854e8a1a543095a5611056
Sergey Pashaev <sergey.pashaev@gmail.com>
2015-09-23 12:19:42 +0400

    minor elfeed list update

--- lisp/configs/elfeed-config.el
+++ lisp/configs/elfeed-config.el
@@ -3,6 +3,9 @@
 (require-or-install 'elfeed)
 
 (setq elfeed-feeds '("http://www.reddit.com/r/emacs/.rss"
-                     "http://planet.emacsen.org/ru/atom.xml"))
+                     "http://planet.emacsen.org/ru/atom.xml"
+                     "http://planet.emacsen.org/atom.xml"
+                     "https://news.ycombinator.com/rss"
+                     "https://meduza.io/rss/all"))
 
 (provide 'elfeed-config)
