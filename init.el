c34c08676084439074fae455811fe6ac9f98e409
Sergey Pashaev <sergey.pashaev@gmail.com>
2016-11-02 12:20:50 +0300

    add final newline enable/disable functions, narrow widen dwim

--- init.el
+++ init.el
@@ -271,6 +271,8 @@
 
 (add-hook 'dired-mode-hook 'psv/dired-hook)
 
+(setq dired-dwim-target t)
+
 ;;; misc
 (defun psv/beginning-of-line-dwim ()
   "Toggles between moving point to the first non-whitespace character, and
@@ -1208,3 +1210,37 @@ buffer is not visiting a file."
 (bind-key "<f5>" 'psv/work-todo)
 (bind-key "<f6>" 'psv/todo)
 
+(defun disable-final-newline ()
+  (interactive)
+  (set (make-local-variable 'require-final-newline) nil))
+
+(defun enable-final-newline ()
+  (interactive)
+  (set (make-local-variable 'require-final-newline) t))
+
+;; http://endlessparentheses.com/emacs-narrow-or-widen-dwim.html
+(defun narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+Dwim means: region, org-src-block, org-subtree, or
+defun, whichever applies first. Narrowing to
+org-src-block actually calls `org-edit-src-code'.
+
+With prefix P, don't widen, just narrow even if buffer
+is already narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning)
+                           (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing
+         ;; command. Remove this first conditional if
+         ;; you don't want it.
+         (cond ((ignore-errors (org-edit-src-code) t)
+                (delete-other-windows))
+               ((ignore-errors (org-narrow-to-block) t))
+               (t (org-narrow-to-subtree))))
+        ((derived-mode-p 'latex-mode)
+         (LaTeX-narrow-to-environment))
+        (t (narrow-to-defun))))
