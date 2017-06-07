ce3d669bdebec94d7a750313b67274fda85cf23e
Sergey Pashaev <sergey.pashaev@gmail.com>
2017-06-07 19:29:05 +0300

    add unfill-paragraph function

--- init.el
+++ init.el
@@ -1283,3 +1283,11 @@ is already narrowed."
 (use-package cmake-mode
   :ensure t)
 
+
+(defun unfill-paragraph (&optional region)
+      "Takes a multi-line paragraph and makes it into a single line of text."
+      (interactive (progn (barf-if-buffer-read-only) '(t)))
+      (let ((fill-column (point-max))
+            ;; This would override `fill-column' if it's an integer.
+            (emacs-lisp-docstring-fill-column t))
+        (fill-paragraph nil region)))
