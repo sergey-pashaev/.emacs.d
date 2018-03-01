e0cd5e97f20029883a89a384a8f88929689a7d40
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-03-01 22:58:44 +0300

    add clang-tidy

--- elisp/lang-cpp.el
+++ elisp/lang-cpp.el
@@ -89,4 +89,10 @@
 
 (add-hook 'c++-mode-hook 'clang-format-buffer-smart-on-save)
 (add-hook 'c-mode-hook   'clang-format-buffer-smart-on-save)
+
+(use-package flycheck-clang-tidy)
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-clang-tidy-setup))
+
 (provide 'lang-cpp)
