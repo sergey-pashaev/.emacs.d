9d2910994f3f8b4adee0079c710ae075a7748558
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-08-13 13:01:32 +0300

    set default prefix for projectile commands

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -104,7 +104,8 @@
 						 (projectile-project-name))
 				       "")))
 
-  (projectile-global-mode))
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode))
 
 (use-package recentf
   :config
