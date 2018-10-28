6435318f6efde8b8cf900a06f716b69023fc8f31
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-10-28 20:15:29 +0400

    add markdown config

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -123,7 +123,12 @@
   (yas-global-mode 1)
   (yas-reload-all))
 
-(use-package markdown-mode)
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "pandoc"))
 
 (use-package elfeed
   :config
