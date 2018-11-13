8252db84f0ab4d04c1dfc131e37e42d73b5a6b5a
Sergey Pashaev <sergey.pashaev@gmail.com>
2018-11-13 12:35:22 +0400

    remove unused scripts

--- sh/ditaa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/Distributives
-wget http://sourceforge.net/projects/ditaa/files/latest/download -O ditaa.zip
-unzip -p ditaa.zip ditaa0_9.jar > ~/bin/ditaa.jar
-chmod +x ~/bin/ditaa.jar
--- sh/plantuml.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/bin
-wget http://sourceforge.net/projects/plantuml/files/plantuml.jar/download -O plantuml.jar
-chmod +x plantuml.jar
