d9f21e33410c6128d3639effd4c4d4d0e08ebae2
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-03-09 10:30:35 +0400

    split c++ config to thinkpad (lsp) & ops (rtags)

--- elisp/hosts/ops.el
+++ elisp/hosts/ops.el
@@ -9,5 +9,40 @@
     :config
     (load-theme 'solarized-light t)))
 
+;;; C++
+ (use-package company-quickhelp
+  :ensure t
+  :config
+  (company-quickhelp-mode))
+
+(use-package company-c-headers :ensure t)
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends '(company-irony-c-headers)))
+
+(use-package rtags
+  :ensure t
+  :config
+  (progn
+    (setq rtags-completions-enabled t)
+    (setq rtags-autostart-diagnostics t)
+    (rtags-enable-standard-keybindings)
+    (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
+    (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)))
+
+(use-package company-rtags
+  :ensure t
+  :config
+  (eval-after-load 'company
+    '(add-to-list
+      'company-backends 'company-rtags)))
+
+(use-package flycheck-rtags :ensure t)
+
+(use-package helm-rtags
+  :ensure t
+  :config
+  (setq rtags-display-result-backend 'helm))
+
 (provide 'ops)
 ;;; ops ends here
--- elisp/hosts/thinkpad.el
+++ elisp/hosts/thinkpad.el
@@ -6,5 +6,20 @@
 (when window-system
   (set-frame-font "Liberation Mono 12" nil t))
 
+;;; C++
+(use-package lsp-mode :ensure t :commands lsp)
+(use-package lsp-ui :ensure t :commands lsp-ui-mode)
+(use-package company-lsp :ensure :commands company-lsp)
+
+(require 'company-lsp)
+(push 'company-lsp company-backends)
+
+(use-package ccls
+  :ensure t
+  :hook ((c-mode c++-mode objc-mode) .
+         (lambda () (require 'ccls) (lsp))))
+
+(use-package helm-lsp :ensure t)
+
 (provide 'thinkpad)
 ;;; thinkpad ends here
