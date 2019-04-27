d062fa369354d9a287da529a596f4f6a329a9dac
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-04-27 15:03:54 +0400

    configure lsp-mode

--- elisp/hosts/thinkpad.el
+++ elisp/hosts/thinkpad.el
@@ -7,25 +7,49 @@
   (set-frame-font "Liberation Mono 12" nil t))
 
 ;;; C++
-(use-package lsp-mode :ensure t :commands lsp)
-(use-package lsp-ui :ensure t :commands lsp-ui-mode)
-(use-package company-lsp :ensure :commands company-lsp)
-
-(require 'company-lsp)
-(push 'company-lsp company-backends)
+(defun psv/lsp-mode-hook ()
+  "My lsp-mode hook."
+  (require 'ccls)
+  (lsp))
 
 (use-package ccls
   :ensure t
-  :hook ((c-mode c++-mode objc-mode) .
-         (lambda () (require 'ccls) (lsp))))
+  :hook
+  ((c-mode c++-mode objc-mode) . psv/lsp-mode-hook))
 
-(use-package helm-lsp :ensure t)
-
-(use-package flycheck-clang-analyzer
+(use-package lsp-mode
   :ensure t
-  :after flycheck
+  :diminish lsp-mode
+  :commands lsp
   :config
-  (flycheck-clang-analyzer-setup))
+  (setq lsp-enable-indentation nil)
+  (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-prefer-flymake nil))
+
+(defun psv/lsp-ui-mode-hook ()
+  "My lsp-ui-mode hooj."
+  (lsp-ui-flycheck-enable 1))
+
+(use-package lsp-ui
+  :ensure t
+  :hook
+  (lsp-mode . lsp-ui-mode)
+  :config
+  (require 'lsp-ui-flycheck)
+  (add-hook 'lsp-after-open-hook 'psv/lsp-ui-mode-hook)
+  (setq lsp-ui-sideline-show-hover nil))
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp
+  :after (:all lsp company)
+  :config
+  (push 'company-lsp company-backends))
+
+(bind-key "C-c h" 'ccls-inheritance-hierarchy lsp-ui-mode-map)
+(bind-key "C-c m" 'ccls-member-hierarchy lsp-ui-mode-map)
+(bind-key "C-c c" 'ccls-call-hierarchy lsp-ui-mode-map)
+(bind-key "C-c <" 'lsp-find-references lsp-ui-mode-map)
 
 (provide 'thinkpad)
 ;;; thinkpad ends here
