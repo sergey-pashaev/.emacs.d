5920dfc1cbafd8cea5a3c1807110e20cdd362e9a
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-04-27 10:23:58 +0400

    add shell-pop package

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -277,5 +277,15 @@
   :config
   (global-diff-hl-mode))
 
+(use-package shell-pop
+  :ensure t
+  :bind (("C-`" . shell-pop))
+  :config
+  (setq shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+  (setq shell-pop-term-shell "/bin/bash")
+  (setq shell-pop-full-span t)
+  ;; need to do this manually or not picked up by `shell-pop'
+ (shell-pop--set-shell-type 'shell-pop-shell-type shell-pop-shell-type))
+
 (provide 'base-extensions)
 ;;; base-extensions.el ends here
