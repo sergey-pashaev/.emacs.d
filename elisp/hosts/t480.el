8c8ca50e99ae3cb2b193ba9f23d75922ec2646bb
Sergey Pashaev <sergey.pashaev@gmail.com>
2019-07-20 14:42:58 +0400

    add lsp-ui-imenu binding

--- elisp/hosts/t480.el
+++ elisp/hosts/t480.el
@@ -58,6 +58,7 @@
 (bind-key "C-c c" 'ccls-call-hierarchy lsp-ui-mode-map)
 (bind-key "C-c <" 'lsp-find-references lsp-ui-mode-map)
 (bind-key "C-c r" 'lsp-rename lsp-ui-mode-map)
+(bind-key "C-c i" 'lsp-ui-imenu lsp-ui-mode-map)
 
 (use-package helm-lsp
   :ensure t
